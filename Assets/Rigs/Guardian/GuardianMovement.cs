357e303218c6d40e827632484f776555b5af9479
Michael L <woodvideogame12@gmail.com>
2021-03-18 19:34:11 -0400

    Animations with Guardian

---
+++ Assets/Rigs/Guardian/GuardianMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardianMovement : MonoBehaviour
+{
+
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = Input.GetAxisRaw("Vertical");
+        anim.SetFloat("CurrSpeed", speed);
+
+        transform.position += transform.forward * speed * Time.deltaTime * 3;
+    }
+}
